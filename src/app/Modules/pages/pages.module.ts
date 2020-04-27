5d384e0e6056e5642dcaad890ccdfa5bb459b3e3
Rupesh <rupesh.graphene@outlook.com>
2020-04-27 12:32:48 +0530

    angular structure

---
+++ src/app/Modules/pages/pages.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import {ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from 'src/app/material-module';
+import { ApiService } from 'src/app/Services/ApiService/api.service';
+import { CommonService } from 'src/app/Services/CommonService/common.service';
+
+
+@NgModule({
+  declarations: [LoginComponent, SignupComponent, HomeComponent, NavbarComponent],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  providers:[ApiService,CommonService]
+})
+export class PagesModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from './Services/ApiService/api.service';
+import { CommonService } from './Services/CommonService/common.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
+  ],
+  providers: [ApiService,CommonService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
